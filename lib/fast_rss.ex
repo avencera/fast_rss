9d88e60d3309ed5f0036b251da6b1daf5c409d09
Praveen Perera <ppraveen25@gmail.com>
2020-02-20 14:56:11 -0500

    Initial commit

---
+++ lib/fast_rss.ex
@@ -0,0 +1,18 @@
+defmodule FastRss do
+  @moduledoc """
+  Documentation for `FastRss`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FastRss.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fast_rss_test.exs
@@ -0,0 +1,8 @@
+defmodule FastRssTest do
+  use ExUnit.Case
+  doctest FastRss
+
+  test "greets the world" do
+    assert FastRss.hello() == :world
+  end
+end
