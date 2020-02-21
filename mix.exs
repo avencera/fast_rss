07b4fde6399fa88cabed2e2e3fa8d787259c3513
Praveen Perera <ppraveen25@gmail.com>
2020-02-21 10:45:12 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FastRss.MixProject do
   def project do
     [
       app: :fast_rss,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
