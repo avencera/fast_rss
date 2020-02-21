473f7161d81c52bbd2b6b1c6df978dbe0cd29694
Praveen Perera <ppraveen25@gmail.com>
2020-02-21 10:24:45 -0500

    Add docs

--- lib/fast_rss.ex
+++ lib/fast_rss.ex
@@ -1,19 +1,24 @@
 defmodule FastRSS do
   @moduledoc """
-  Documentation parse RSS quickly using a Rust nif.
+  Parse RSS quickly using a Rust nif.
   """
 
   defmodule Native do
+    @doc false
     use Rustler, otp_app: :fast_rss, crate: "fastrss"
 
     # When your NIF is loaded, it will override this function.
     def parse(_a), do: :erlang.nif_error(:nif_not_loaded)
   end
 
+  @doc """
+  Parse a RSS string into a map
+  """
+  @spec parse(String.t()) :: {:ok, map()} | {:error, String.t()}
   def parse(""), do: {:error, "Cannot parse blank string"}
 
-  def parse(string) when is_binary(string) do
-    with {:ok, json} <- Native.parse(string) do
+  def parse(rss_string) when is_binary(rss_string) do
+    with {:ok, json} <- Native.parse(rss_string) do
       Jason.decode(json, keys: :atoms)
     end
   end
