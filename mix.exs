8416b06a36c8b259a1d99da0643bc2fac775b604
Praveen Perera <ppraveen25@gmail.com>
2020-02-21 11:20:47 -0500

    Relax elixir requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FastRss.MixProject do
     [
       app: :fast_rss,
       version: "0.1.3",
-      elixir: "~> 1.10",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       # hex
