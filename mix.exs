e9192fbf3a984c18e9230985430e8e073d519842
Praveen Perera <ppraveen25@gmail.com>
2020-02-24 13:54:03 -0500

    Change minimum elixir version down to 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FastRss.MixProject do
     [
       app: :fast_rss,
       version: "0.3.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       # hex
