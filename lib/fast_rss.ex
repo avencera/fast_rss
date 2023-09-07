19eafd522f85cccaaac7c4eb5ffaacb10e36452e
Praveen Perera <praveen@avencera.com>
2023-09-07 18:19:26 -0400

    Add ability to parse atom feeds
    Added new `parse_rss` and `parse_atom`, deprecate `parse` function

--- lib/fast_rss.ex
+++ lib/fast_rss.ex
@@ -18,22 +18,49 @@ defmodule FastRSS do
       version: version
 
     # When your NIF is loaded, it will override this function.
-    def parse(_a), do: :erlang.nif_error(:nif_not_loaded)
+    def parse_rss(_a), do: :erlang.nif_error(:nif_not_loaded)
+    def parse_atom(_a), do: :erlang.nif_error(:nif_not_loaded)
   end
 
   @doc """
   Parse a RSS string into a map.
   """
   @spec parse(String.t()) :: {:ok, map()} | {:error, String.t()}
+  @since "0.5.0"
+  @deprecated "Use FastRSS.parse_rss/1 instead"
   def parse(""), do: {:error, "Cannot parse blank string"}
+  def parse(rss_string) when is_binary(rss_string), do: parse_rss(rss_string)
+  def parse(_something_else), do: {:error, "RSS feed must be passed in as a string"}
 
-  def parse(rss_string) when is_binary(rss_string) do
+  @doc """
+  Parse a RSS string into a map.
+  """
+  @spec parse_rss(String.t()) :: {:ok, map()} | {:error, String.t()}
+  @since "0.5.0"
+  def parse_rss(""), do: {:error, "Cannot parse blank string"}
+
+  def parse_rss(rss_string) when is_binary(rss_string) do
     rss_string
-    |> Native.parse()
+    |> Native.parse_rss()
     |> map_to_tuple()
   end
 
-  def parse(_somethig_else), do: {:error, "RSS feed must be passed in as a string"}
+  def parse_rss(_something_else), do: {:error, "RSS feed must be passed in as a string"}
+
+  @doc """
+  Parse a Atom string into a map.
+  """
+  @spec parse_atom(String.t()) :: {:ok, map()} | {:error, String.t()}
+  @since "0.5.0"
+  def parse_atom(""), do: {:error, "Cannot parse blank string"}
+
+  def parse_atom(atom_string) when is_binary(atom_string) do
+    atom_string
+    |> Native.parse_atom()
+    |> map_to_tuple()
+  end
+
+  def parse_atom(_something_else), do: {:error, "Atom feed must be passed in as a string"}
 
   defp map_to_tuple(%{"Ok" => map}), do: {:ok, map}
   defp map_to_tuple({:ok, map}), do: {:ok, map}
